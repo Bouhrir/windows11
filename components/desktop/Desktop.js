476a4445b107a1889bbe3d9b5e84f360d75d6acc
miroka__ <bouhrir123@gmail.com>
2025-08-31 14:29:35 +0100

    modify time to AM PM format

--- components/desktop/Desktop.js
+++ components/desktop/Desktop.js
@@ -10,12 +10,18 @@ export default function Desktop() {
 
     return () => clearInterval(interval);
   }, []);
+  const formattedTime = time.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true, // Ensures AM/PM format
+  });
 
   return (
     <div className="min-h-screen w-screen bg-black flex flex-col">
       <div className="relative">
         <p className="absolute left-1/2 -translate-x-1/2 text-8xl mt-9 inter">
-          {time.getHours()}:{time.getMinutes().toString().padStart(2, '0')}:{time.getSeconds().toString().padStart(2, '0')}
+			{formattedTime}
         </p>
         <img src="/wallpaper.jpg" alt="Wallpaper" className="h-auto w-full object-cover" />
       </div>
